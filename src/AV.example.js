b903a71d640e22574e26080ee022ea214f2f2d63
Zhuo Zhang <contrary.zhuo@gmail.com>
2016-11-27 22:32:28 +0800

    [init] leancloud

---
+++ src/AV.example.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+
+// Change it to yours and rename this file to 'AV.js'
+const APP_ID = 'xxx'
+const APP_KEY = 'xxx'
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
